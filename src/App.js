08b9713df0322a2fe663c7f91fc14745f7eb050c
juancarlosbs <juanrov_@hotmail.com>
2021-04-13 15:17:47 -0300

    feat: ExpenseItem with props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
+import ExpenseItem from './components/ExpenseItem'
+
 function App() {
+  const expenses = [{
+    id: 'e1',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 0, 12)
+  }]
+
+  console.log(expenses[0].date)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started</h2>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
     </div>
   );
 }
